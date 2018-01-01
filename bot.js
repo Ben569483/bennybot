978c000dc8a98fda2622697371e1d5bd46e6cc1e
Ben569483 <35010334+Ben569483@users.noreply.github.com>
2018-01-01 20:11:05 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+     console.log('I am ready!');
+     });
+     
+     client.on('message', message => {
+          if (message.content === 'ping') {
+             message.reply('pong');
+          )
+          });
+          
+          // THIS MUST BE THIS WAY
+          client.login(process.env.Mzk3NDczNDYwNDI0ODAyMzA2.DSwkmw.Z2MwOQm2voJ2RVLcoBRGoVLvgOM);
